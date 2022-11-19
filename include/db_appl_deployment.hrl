e880f4a64002bd65f4f62a3466389d37f8ca2326
joq62 <joakim.leche@gmail.com>
2022-11-20 00:13:23 +0100

    ud

--- include/db_appl_deployment.hrl
+++ include/db_appl_deployment.hrl
@@ -1,6 +1,9 @@
+-define(ApplDeploymentDir,"application_deployments").
+
 -define(TABLE,appl_deployment).
 -define(RECORD,?TABLE).
 -record(?RECORD,{
+		 spec_id,
 		 appl_name,
 		 vsn,
 		 num_instances,
